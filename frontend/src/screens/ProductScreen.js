3dde4a99104ffbd7c4c74551ba824f6cf965ac7d
Alihan <82150661+alienslimeson@users.noreply.github.com>
2023-08-12 00:05:28 +0300

    Create Cart Screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -9,7 +9,7 @@ import {
   ListGroupItem,
   Row,
 } from 'react-bootstrap';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import Rating from '../components/Rating';
 import { Helmet } from 'react-helmet-async';
 import { LoadingBox } from '../components/LoadingBox';
@@ -31,6 +31,7 @@ const reducer = (state, action) => {
 };
 
 const ProductScreen = () => {
+  const navigate = useNavigate();
   const params = useParams();
   const { slug } = params;
 
@@ -63,6 +64,7 @@ const ProductScreen = () => {
       type: 'CART_ADD_ITEM',
       payload: { ...product, quantity },
     });
+    navigate('/cart')
   };
 
   return loading ? (
