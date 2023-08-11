5da5fd6b25e10459332eed89310a6fb8c3d1739b
Alihan <82150661+alienslimeson@users.noreply.github.com>
2023-08-11 11:03:30 +0300

    create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Amazona</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
