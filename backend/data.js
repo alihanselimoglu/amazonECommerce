003938615e4ddc650ce1e34a9a4786c7438094bb
Alihan <82150661+alienslimeson@users.noreply.github.com>
2023-08-12 16:34:03 +0300

    Create Sample Users

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,21 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'alihan',
+      email: 'admin@example,com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'John',
+      email: 'user@example,com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
+
   products: [
     {
       name: 'Nike Slim shirt',
--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import Product from '../models/ProductModel.js';
 import data from '../data.js';
+import User from '../models/userModel.js';
 
 const seedRouter = express.Router();
 
 seedRouter.get('/', async (req, res) => {
-    await Product.deleteMany({})
+  await User.deleteMany({});
+  const createdUsers = await User.insertMany(data.users);
+  await Product.deleteMany({});
   const createdProducts = await Product.insertMany(data.products);
-  res.send({ createdProducts });
+  res.send({ createdProducts, createdUsers });
 });
 export default seedRouter;
