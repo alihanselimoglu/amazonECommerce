1456bc9b68dd887bfe7270b4c157b139b7bf8c19
Alihan <82150661+alienslimeson@users.noreply.github.com>
2023-08-12 12:32:19 +0300

    Create Sample Products

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import seedRouter from './routes/seedRoutes.js';
+import productRouter from './routes/productRoutes.js';
 
 dotenv.config();
 
@@ -14,30 +16,14 @@ mongoose.connect(process.env.MONGODB_URI, {
   console.log(error);
 });
 
-
 const app = express();
 
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
-});
+app.use(express.json());
 
-app.get('/api/products/slug/:slug', (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product not found!' });
-  }
-});
+app.use('/api/seed', seedRouter);
+app.use('/api/products', productRouter);
 
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product not found!' });
-  }
-});
+
 
 const port = process.env.PORT || 5000;
 
