9e1f8db2e3a9471d4d27363a7436be8a1a9971b3
Alihan <82150661+alienslimeson@users.noreply.github.com>
2023-08-12 10:34:15 +0300

    Connect To MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,19 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => { 
+  console.log("Connected to MongoDB...");
+}).catch((error) => {
+  console.log(error);
+});
+
 
 const app = express();
 
